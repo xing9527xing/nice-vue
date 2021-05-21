4427950b3d779689f158124e49fadbe939c9d504
13803990170 <13803990170@163.com>
2021-05-21 17:48:06 +0800

    vuetree

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <!--引用部分（vue文件）-->
+    <tag-select v-model="value" :allTags="allTags" />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import tagSelect from "./components/select";
+export default {
+  data() {
+    return {
+      value: [],
+      allTags: [
+        {
+          id: "a",
+          label: "a",
+          children: [
+            {
+              id: "aa",
+              label: "aa",
+            },
+            {
+              id: "ab",
+              label: "ab",
+            },
+          ],
+        },
+        {
+          id: "b",
+          label: "b",
+        },
+        {
+          id: "c",
+          label: "c",
+        },
+      ],
+    };
+  },
+  components: {
+    tagSelect,
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style scoped lang="scss">
 </style>
